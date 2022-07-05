ccec1af901b609707e26490eeef5f079a112849e
Carlos Carmona <caneladelmoro@gmail.com>
2022-07-05 17:54:12 -0300

    P-SALESSYSTEM: add class Product and Order

--- proyecto sistema de ventas/Order.js
+++ proyecto sistema de ventas/Order.js
@@ -1,3 +1,35 @@
 class Order{
-    
+    static countOrders = 0;
+
+    static get MAX_PRODUCTS(){
+        return 5;
+    }
+
+    constructor(){
+        this._idOrder = ++Order.countOrders;
+        this._products = [];
+        this._countAddedProducts = 0;
+
+    }
+
+    get idOrder(){
+        return this._idOrder;
+    }
+
+    addProduct(product){
+        if (this._products.length < Order.MAX_PRODUCTS){
+            this._products.push(product);
+            //this._products[this._countAddedProducts++] = product;
+        }
+        else{
+            console.log('No se pueden aderir mas productos');
+        }
+    }
+
+    calculateTotal(){
+        let totalSales = 0;
+        for(let product of this._products){
+            totalSales += product.price
+        }
+    }
 }
--- proyecto sistema de ventas/Product.js
+++ proyecto sistema de ventas/Product.js
@@ -1,3 +1,35 @@
 class Product{
-    
+    static countProducts = 0;
+
+    constructor(name, price){
+        this._idProduct = ++Product.countProducts;
+        this._name = name;
+        this._price = price;
+    }
+
+    get idProduct(){
+        return this._idProduct;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    get price(){
+        return this._price;
+    }
+
+    set price(price){
+        this._price = price;
+    }
+
+    toString(){
+        return `${this._idProduct} 
+                ${this._name} 
+                ${this._price}` ;
+    } 
 }
