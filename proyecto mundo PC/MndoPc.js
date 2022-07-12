f1d1cb16c11f231ce925a95bee9f1f3b34a452e0
Carlos Carmona <caneladelmoro@gmail.com>
2022-07-11 23:53:44 -0300

    "PROYECTO MUNDO PC: class inputModules"

---
+++ proyecto mundo PC/MndoPc.js
@@ -0,0 +1,20 @@
+class InputModules{
+    constructor(inputType, brand){
+        this._inputType = inputType;
+        this._brand = brand;
+    }
+    get inputType(){
+        return this._inputType;
+    }
+    set inputType(inputType){
+        this._inputType = inputType;
+    }
+
+    get brand(){
+        return this._brand;
+    }
+    set brand(brand){
+        this._brand = brand;
+    }
+     
+}
